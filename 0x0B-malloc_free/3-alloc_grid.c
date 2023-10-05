0af0d5c31c0fa54f28d2d523a864212236b6e4b7
bilal muili <muilibilal2019@gmail.com>
2023-10-05 04:18:13 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,3 +26,15 @@ int **alloc_grid(int width, int height)
 		if (array[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+
+	return (array);
+}
