5c25f8ce6de08c048c4dca87cdff18c6c941bd4f
bilal muili <muilibilal2019@gmail.com>
2023-10-10 16:39:15 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef DOG
 #define DOG
 
 /**
@@ -17,3 +18,13 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
